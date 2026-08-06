ce75328ec60437ed0d9dedc486b63efde02f355a
jahirul_islam <jisjis115@gmail.com>
2026-08-06 18:35:55 +0600

    make login , register  form

---
+++ src/pages/register_page/Register.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { GoogleLogin } from '../../components/GoogleLogin'
+import { Link } from 'react-router'
+import { IoEyeOffSharp, IoEyeSharp } from 'react-icons/io5'
+
+export const Register = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    return (
+        <div className="w-8/12 mx-auto pt-10 md:pt-10 space-y-4">
+            <h1 className='text-5xl font-extrabold text-secondary text-center md:text-left'>Create An Account</h1>
+            <p className='text-center md:text-left text-secondary font-semibold'>Register With Zap Shift</p>
+            <fieldset className="fieldset">
+                <label className="label ">Email</label>
+                <input type="email" className="input w-full font-bold" name='email' placeholder="Email" />
+                <label className="label ">Password</label>
+                <div className='relative flex items-center'>
+                    <input type={showPassword ? "text" : "password"} className="input w-full font-bold " placeholder="Password" name='pass' />
+                    <IoEyeOffSharp
+                        onClick={() => setShowPassword(!showPassword)}
+                        className={`absolute cursor-pointer text-xl transition-all duration-200 right-2 ${showPassword ? "opacity-0" : "opacity-100"}`}
+                    />
+                    <IoEyeSharp
+                        onClick={() => setShowPassword(!showPassword)}
+                        className={`absolute cursor-pointer text-xl transition-all duration-200 right-2 ${showPassword ? "opacity-100" : "opacity-0"}`}
+                    />
+                </div>
+                <Link  className='underline text-primary font-semibold'>Forget Password ?</Link>
+                <button className="btn btn-primary text-xl mt-4 w-full text-secondary font-semibold ">Register</button>
+                <p>Already Have An Account ?<Link to='/login' className='underline text-primary font-semibold'>Loging</Link></p>
+            </fieldset>
+            <GoogleLogin></GoogleLogin>
+        </div>
+    )
+}
