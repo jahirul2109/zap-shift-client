87806de734ed71ba4099db83623a28c9ce6a06ea
jahirul_islam <jisjis115@gmail.com>
2026-08-08 23:35:48 +0600

    added page

---
+++ src/pages/Auth/forgot_page/Forgot.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export const Forgot = () => {
+    return (
+        <div className="w-8/12 mx-auto pt-10 md:pt-10 space-y-4">
+            <h1 className='text-5xl font-extrabold text-secondary text-center md:text-left'>Forgot Password</h1>
+            <p className='text-center md:text-left text-secondary font-semibold'>Enter your email address and we’ll send you a reset link.</p>
+            <fieldset className="fieldset">
+                <label className="label ">Email</label>
+                <input type="email" className="input w-full font-bold" name='email' placeholder="Email" />
+                <button className="btn btn-primary text-xl mt-4 w-full text-secondary font-semibold ">Send</button>
+                <p>Remember your Password ?<Link to='/login' className='underline text-primary font-semibold'> Login</Link></p>
+            </fieldset>
+        </div>
+    )
+}
---
+++ src/pages/Auth/login_page/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router'
+import { IoEyeOffSharp, IoEyeSharp } from 'react-icons/io5'
+import { useForm } from 'react-hook-form'
+import useAuth from '../../../hook/useAuth'
+import { GoogleLogin } from '../../../components/GoogleLogin'
+
+export const Login = () => {
+    const { loginWithEmailPassword } = useAuth();
+    const [showPassword, setShowPassword] = useState(false)
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state || '/';
+    console.log(location)
+
+    const { handleSubmit, register, reset } = useForm()
+    const handelLogin = (data) => {
+        const { email, pass } = data;
+        loginWithEmailPassword(email, pass)
+            .then(result => {
+                console.log(result)
+                navigate(from)
+            })
+    }
+    return (
+        <div className="w-8/12 mx-auto pt-10 md:pt-10 space-y-4">
+            <h1 className='text-5xl font-extrabold text-secondary text-center md:text-left'>Welcome Back</h1>
+            <p className='text-center md:text-left text-secondary font-semibold'>Login With Zap Shift</p>
+            <form className="fieldset" onSubmit={handleSubmit(handelLogin)}>
+                <label className="label ">Email</label>
+                <input type="email" {...register("email")} className="input w-full font-bold" name='email' placeholder="Email" />
+                <label className="label ">Password</label>
+                <div className='relative flex items-center'>
+                    <input type={showPassword ? "text" : "password"} {...register("pass")} className="input w-full font-bold " placeholder="Password" name='pass' />
+                    <IoEyeOffSharp
+                        onClick={() => setShowPassword(!showPassword)}
+                        className={`absolute cursor-pointer text-xl transition-all duration-200 right-2 ${showPassword ? "opacity-0" : "opacity-100"}`}
+                    />
+                    <IoEyeSharp
+                        onClick={() => setShowPassword(!showPassword)}
+                        className={`absolute cursor-pointer text-xl transition-all duration-200 right-2 ${showPassword ? "opacity-100" : "opacity-0"}`}
+                    />
+                </div>
+                <Link to='/forget_password' className='underline text-primary font-semibold'>Forget Password ?</Link>
+                <button className="btn btn-primary text-xl mt-4 w-full text-secondary font-semibold ">Login</button>
+                <p>Don't Have An Account ? <Link to='/register' state={location.state} className='underline text-primary font-semibold'>Register</Link></p>
+            </form>
+            <GoogleLogin state={location.state} ></GoogleLogin>
+        </div>
+    )
+}
