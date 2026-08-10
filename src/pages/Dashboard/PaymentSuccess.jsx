bb249ca4bfeb0b79ce76f9217ca265b938eb2668
jahirul_islam <jisjis115@gmail.com>
2026-08-11 01:38:13 +0600

    add parcel trackingId

--- src/pages/Dashboard/PaymentSuccess.jsx
+++ src/pages/Dashboard/PaymentSuccess.jsx
@@ -1,16 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router'
 import { useAxiousSecoure } from '../../hook/useAxiousSecoure'
 
 export const PaymentSuccess = () => {
     const axiosInstence = useAxiousSecoure();
-    const [searchParams] = useSearchParams()
+    const [searchParams] = useSearchParams();
+    const [paymentInfo, setPaymentInfo] = useState({})
     const sessionId = searchParams.get("session_id");
     console.log(sessionId);
     useEffect(() => {
         axiosInstence.patch(`/payment-verification?session_id=${sessionId}`)
+            .then(res => {
+                console.log(res.data)
+                setPaymentInfo({
+                    TransactionId: res.data.TransactionId,
+                    trackingId: res.data.trackingId
+                })
+            })
     }, [sessionId, axiosInstence])
     return (
-        <div>PaymentSuccess</div>
+        <div>
+            <h1 className="text-xl">Payment Successfull</h1>
+            <p>Transaction ID : <span className='text-blue underline'> {paymentInfo.TransactionId}</span></p>
+            <p>Parcel Tracking ID : <span className='text-blue underline'> {paymentInfo.trackingId}</span></p>
+        </div>
     )
 }
