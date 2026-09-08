15be420deb6d605cf81f6694cf28cf637151ef16
jahirul_islam <jisjis115@gmail.com>
2026-09-09 01:24:42 +0600

    66th commit (server url change)

--- src/hook/useAxiousSecoure.jsx
+++ src/hook/useAxiousSecoure.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router";
 
 
 const axiousSecoure = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://zap-shift-server-plum-zeta.vercel.app'
 })
 
 export const useAxiousSecoure = () => {
